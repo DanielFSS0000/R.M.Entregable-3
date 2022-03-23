7b58d060cac54e51554e12ebdd2939875b79032c
DanielFSS0000 <daniel97062@gmail.com>
2022-03-22 23:08:16 -0500

    subiendo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ResidentList from './components/ResidentList';
+import SearchBox from './components/SearchBox';
+import LocationInfo from './components/LocationInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ location, setLocation ] = useState({});
+    const [ currentPage, setCurrentPage ] = useState(1);
+    const [ currentList, setCurrentList ] = useState(0);
+
+    useEffect(() => {
+        const random = Math.floor(Math.random() * 126) + 1;
+
+        axios.get(`https://rickandmortyapi.com/api/location/${random}/`)
+            .then(res => {
+                setLocation(res.data)
+                setCurrentPage(1);
+                setCurrentList(0);
+            })
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Rick and Morty Pedia</h1>
+            <SearchBox 
+                setLocation={setLocation}
+                setCurrentPage={setCurrentPage}
+                setCurrentList={setCurrentList}
+            />
+            <LocationInfo 
+                name={location.name}
+                type={location.type}
+                dimension={location.dimension}
+                population={location.residents?.length}
+            />    
+            <ResidentList 
+                residents={location.residents}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                currentList={currentList}
+                setCurrentList={setCurrentList}
+            />
+        </div>
+    );
 }
 
 export default App;
