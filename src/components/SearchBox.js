4ce28a8d2efdb835b7db37f3f9784ea7242dd2f2
DanielFSS0000 <daniel97062@gmail.com>
2022-03-23 17:04:41 -0500

    ready for deploy

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import rick from '../images/rick.png';
 import morty from '../images/morty.png' 
 
 const SearchBox = ({ setLocation, setCurrentPage, setCurrentList }) => {
     const [ search, setSearch ] = useState("");
+    const [locations, setLocations] = useState([]);
 
     const searchLocation = () => {
         axios.get(`https://rickandmortyapi.com/api/location/${search}/`)
@@ -14,7 +15,18 @@ const SearchBox = ({ setLocation, setCurrentPage, setCurrentList }) => {
                 setCurrentList(0);
             })
     }
+    useEffect(() => {
+        if(search){
+            axios
+            .get(`https://rickandmortyapi.com/api/location?name=${search}`)
+            .then(res => setLocations(res.data.results))
+            .catch(error => console.log(error))
+        }else{
+            setLocations([]);
+        }
+    }, [search])
 
+    
     return (
         <div className='search-box'>
             <img src={morty} alt="Morty image" />
@@ -23,9 +35,12 @@ const SearchBox = ({ setLocation, setCurrentPage, setCurrentList }) => {
                     type="text" 
                     onChange={e => setSearch(e.target.value)} 
                     value={search}
-                    placeholder='Ingresa el ID de la ubicación que deseas buscar'
+                    placeholder='Ingrese la #ID de la ubicación para buscar'
                 />
                 <button className='button' onClick={searchLocation}>Search</button>
+                <ul>
+                    {locations?.map(location => <li>{location.name}</li>)}
+                </ul>
             </div>
             <img className='rick' src={rick} alt='Rick image'/>
         </div>
